4e258adb707c2238ef52ac3da576534de0cffa92
marielacp <marielacp@outlook.com.pe>
2018-01-03 14:39:55 -0500

    añadiendo actualizaciones

---
+++ js/data.js
@@ -0,0 +1,103 @@
+var data = [
+  {
+    nombre: 'Germinando',
+    visit: ' Nos puedes encontrar en',
+    direccion: 'Av. Almte. Miguel Grau 209A Lima-Barranco ',
+    telefono: '(01) 3000602  ',
+    horarios: 'L-S 10:00 a 16:00, D 09:00 - 12:00',
+    tags: 'vegano',
+    img: '../assets/img/gv.jpg',
+    platos: [
+      {
+        'nombrePlato': 'tacutacu',
+        'descripcion': 'rico',
+        'img': '../assets/img/yv.jpg',
+      },
+      {
+        'nombrePlato': 'fsfsdfsdf',
+        'descripcion': 'rico',
+        'img': '../assets/img/yv.jpg',
+      }
+    ]
+
+  },
+  {
+    nombre: 'Yamil Vegan',
+    visit: ' Nos puedes encontrar en',
+    direccion: 'Jirón Domingo Casanova 254 Lince',
+    horarios: 'L-S 10:00 a 16:00, D 09:00 - 12:00',
+    tags: 'vegano',
+    img: '../assets/img/yv.jpg',
+    platos: [{
+      'nombrePlato': 'xx',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    },
+    {
+      'nombrePlato': 'yy',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    }
+    ]
+  },
+  {
+    nombre: 'Abdiel Vegan',
+    visit: ' Nos puedes encontrar en',
+    direccion: 'Teodoro Cárdenas 281 Cercado de Lima ',
+    telefono: '(01) 3000602',
+    horarios: '  L-S 10:00 a 16:00, D 09:00 - 12:00',
+    tags: 'vegetariano',
+    img: '../assets/img/av.jpg',
+    platos: [{
+      'nombrePlato': 'tacu tacu',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    },
+    {
+      'nombrePlato': 'fsfsdfsdf',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    }
+    ]
+  },
+  {
+    nombre: 'Sagra Restaurant',
+    visit: ' Nos puedes encontrar en',
+    direccion: 'Jirón Domingo Casanova 254',
+    telefono: '(01) 3000602',
+    horarios: 'L-S 10:00 a 16:00, D 09:00 - 12:00',
+    tags: 'vegetariano',
+    img: '../assets/img/sr.png',
+    platos: [{
+      'nombrePlato': 'tacu tacu',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    },
+    {
+      'nombrePlato': 'fsfsdfsdf',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    }
+    ]
+  },
+  {
+    nombre: 'Salud y Vida',
+    visit: ' Nos puedes encontrar en',
+    direccion: 'Calle Gral Recavarren 156 Miraflores Lima',
+    telefono: '(01) 3000602',
+    horarios: 'L-S 10:00 a 16:00, D 09:00 - 12:00',
+    tags: 'vegano',
+    img: '../assets/img/sv.png',
+    platos: [{
+      'nombrePlato': 'tacu tacu',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    },
+    {
+      'nombrePlato': 'fsfsdfsdf',
+      'descripcion': 'rico',
+      'img': '../assets/img/yv.jpg',
+    }
+    ]
+  }
+];
